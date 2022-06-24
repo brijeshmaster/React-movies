a06292cc1e4679e7ded85871cdcb37bd571d2240
brijeshmaster <brijeshkumarmaisuriya@gmail.com>
2022-06-24 19:37:21 +0200

    change additional

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import Card from './Card';
-ReactDOM.render(<App />, document.getElementById("root"));
 
-//  import React from "react"
-//  import ReactDOM from "react-dom"
-//  import App from './App'
-//  ReactDOM.render(
-//   <App/>,
-//     document.getElementById('root')
-//  )
+ReactDOM.render(
+<App />, 
+document.getElementById("root"));
+
+
